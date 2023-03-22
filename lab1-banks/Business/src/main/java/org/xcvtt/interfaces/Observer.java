8319fb6916dbcc6447c377a806ca783ac6dee29e
exclide <exclide1@gmail.com>
2023-03-22 10:23:54 +0300

    Delete lab1-banks directory

--- lab1-banks/Business/src/main/java/org/xcvtt/interfaces/Observer.java
+++
@@ -1,9 +0,0 @@
-package org.xcvtt.interfaces;
-
-/**
- * Реализация паттерна наблюдатель
- * @param <T> - контекст, о которым хотим быть оповощены
- */
-public interface Observer<T> {
-    void update(T context);
-}
